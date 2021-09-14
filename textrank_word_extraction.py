aaae268c42908aed1e14ed74e847f33bf51a9e73
Xiuhao Ding(Will) <70461543+willDDDD@users.noreply.github.com>
2021-09-13 22:19:21 -0500

    Update textrank_word_extraction.py

--- textrank_word_extraction.py
+++ textrank_word_extraction.py
@@ -115,6 +115,6 @@ def cal_error(s2, s1):
         t.append(abs(s2[i] - s1[i]))
     return t
 
-
+# Test data
 r = textrank_word('Compatibility of systems of linear constraints over the set of natural numbers. Criteria of compatibility of a system of linear Diophantine equations, strict inequations, and nonstrict inequations are considered. Upper bounds for components of a minimal set of solutions and algorithms of construction of minimal generating sets of solutions for all types of systems are given. These criteria and the corresponding algorithms for constructing a minimal supporting set of solutions can be used in solving all the considered types systems and systems of mixed types.')
 print(r)
